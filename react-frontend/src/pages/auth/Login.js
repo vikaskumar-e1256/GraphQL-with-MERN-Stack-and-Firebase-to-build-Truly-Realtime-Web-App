ab57f6f377a50c79878f9f613f131305d7600a36
Nishant <vikaskumar.e1256@gmail.com>
2024-09-05 12:22:09 +0530

    Login with firebase

--- react-frontend/src/pages/auth/Login.js
+++ react-frontend/src/pages/auth/Login.js
@@ -1,15 +1,49 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
+import { getAuth, signInWithEmailAndPassword, GoogleAuthProvider } from "firebase/auth";
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import { AuthContext } from '../../context/authContext';
+
 
 function Login(props)
 {
+    const provider = new GoogleAuthProvider();
+    
     const [loading, setLoading] = useState(false);
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const auth = getAuth();
+    const { dispatch } = useContext(AuthContext);
+    let history = useNavigate();
 
     const handleSubmit = (e) =>
     {
         e.preventDefault();
         // Handle login logic here
+        signInWithEmailAndPassword(auth, email, password)
+            .then(async (userCredential) =>
+            {
+                // Signed in
+                const user = userCredential.user;
+                const idTokenResult = await user.getIdTokenResult();
+
+                dispatch({
+                    type: 'LOGGED_IN_USER',
+                    payload: {
+                        email: user.email,
+                        token: idTokenResult.token,
+                    },
+                });
+                history('/');
+            })
+            .catch((error) =>
+            {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                toast.error(errorMessage);
+
+            });
+        setLoading(false);
     };
 
     return (
