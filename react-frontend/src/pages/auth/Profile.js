6b9d396dc71b7062ca5d115b7996f251123d3509
Nishant <vikaskumar.e1256@gmail.com>
2024-09-07 22:24:25 +0530

    call profile query via react

--- react-frontend/src/pages/auth/Profile.js
+++ react-frontend/src/pages/auth/Profile.js
@@ -1,9 +1,45 @@
-import React from 'react';
+import React, { useMemo, useState } from 'react';
+import { gql, useQuery, useLazyQuery } from '@apollo/client';
 
-function Profile(props) {
+// GraphQL query to fetch posts
+const GET_USER_INFO = gql`
+  query Query {
+    profile {
+        _id
+        username
+        email
+        name
+        images {
+            url
+            public_id
+        }
+        about
+        createdAt
+        updatedAt
+    }
+  }
+`;
+
+function Profile(props)
+{
+    const [values, setValues] = useState({
+        username: '',
+        name: '',
+        email: '',
+        about: '',
+        images: ''
+    });
+
+    const [loading, setLoading] = useState(false);
+
+    // Use Apollo Client's useQuery hook to fetch data
+    const { error, data } = useQuery(GET_USER_INFO);
+
+    useMemo(() => { }, []);
+
     return (
-        <div>
-           Profile...
+        <div className='container pt-5'>
+           {JSON.stringify(data)}
         </div>
     );
 }
